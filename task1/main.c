9cce3412bbd41435f4cb8f42709bfc5306f15fa8
Shane Harding <sharding@tcd.ie>
2015-01-27 14:11:18 +0000

    first commit

---
+++ task1/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+
+int main(int argc, char **argv){
+
+  int nflag, opt, num_cities;
+
+  srand48(time(NULL));
+
+  nflag=0;
+
+  while((opt=getopt(argc,argv,"n:"))!=-1){
+    switch(opt){
+
+    case 'n':
+      nflag=1;
+      num_cities=atoi(optarg);
+
+    }
+  }
+
+  return 0;
+}
